a11713ec301d61b33c02e3f851ebe5bce44b9352
numan-iftikhar <numaniftikhar300@gmail.com>
2022-07-18 21:00:55 +0500

    add navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <section className="nav-wrapper">
+      <div className="nav-left">
+        <div className="nav-name">Numan</div>
+        <span>toggle</span>
+      </div>
+
+      <div className="nav-right">
+        <div className="nav-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Testimonial</li>
+          </ul>
+        </div>
+        <button className="button">Contact Us</button>
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;
